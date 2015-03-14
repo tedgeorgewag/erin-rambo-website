2d5acad02931228438250953fcfef857445bdf65
Teddy Georgia Waggy <teddygeorgiawaggy@gmail.com>
2015-03-13 22:16:03 -0500

    SEO / gallery updates
    Altered meta tags; added sitemap; improved scrolling gallery nav
    functionality

--- behave.js
+++ behave.js
@@ -1,20 +1,30 @@
-//Use the gallery nav a's to move left / right through portfolio
+//Animate gallery scroller left or right with less / more nav buttons
 $(document).ready(function() {
+	//Make array of images
+	var galImgArray = $('.gal-img').toArray();
+	var i = 0;
+
 	$('#scroll-right').click(function() {
+		//Use width of currently viewed image, scrollbar position and room for margin
 		$('#gallery').animate({
-			scrollLeft: $('.gal-img').width() + $('#gallery').scrollLeft()
-		}, 750); 
+			scrollLeft: $(galImgArray[i]).width() + $('#gallery').scrollLeft() + 15
+		}, 750);
+		//Iterate to next image
+		if (i < $(galImgArray).length) {
+			i++;
+		};
 	});
-	
+
 	$('#scroll-left').click(function() {
+		//Iterate to previous image
+		if (i > 0) {
+			i--;
+		};
+		//Use width of currently viewed image, scrollbar position and room for margin
 		$('#gallery').animate({
-			scrollLeft: $('#gallery').scrollLeft() - $('.gal-img').width()
+			scrollLeft: $('#gallery').scrollLeft() - $(galImgArray[i]).width() - 15
 		}, 750);
 	});
 });
-
-
-
-
 
 
